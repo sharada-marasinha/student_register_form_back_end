27eae26db69246dca0285573a165f8558b8c85fb
sharada-marasinha <sharadamarasinha@gmail.com>
2023-08-23 21:32:07 +0530

    fix bug

---
+++ src/main/java/edu/icet/config/JPAConfig.java
@@ -0,0 +1,52 @@
+package edu.icet.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "edu.icet")
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+public class JPAConfig {
+    @Autowired
+    Environment env;
+
+
+    @Bean
+    public DataSource dataSource() throws NamingException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(env.getRequiredProperty("my.app.url"));
+        dataSource.setUsername(env.getRequiredProperty("my.app.username"));
+        dataSource.setPassword(env.getRequiredProperty("my.app.password"));
+        dataSource.setDriverClassName(env.getRequiredProperty("my.app.driverClassName"));
+        return dataSource;
+
+//        return (DataSource) new JndiTemplate().lookup("java:comp/env/jdbc/pool");
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabasePlatform(env.getRequiredProperty("my.app.dialect"));
+        vendorAdapter.setDatabase(Database.MYSQL);
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setGenerateDdl(true);
+        return vendorAdapter;
+    }
+
+}
