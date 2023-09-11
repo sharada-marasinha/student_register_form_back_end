adec1138ff56425de89e912fbf76a1e85a5fa1a7
sharada-marasinha <sharadamarasinha@gmail.com>
2023-09-11 22:43:43 +0530

    adding project Final

--- src/main/java/edu/icet/repository/NativeStudentRepository.java
+++
@@ -1,4 +0,0 @@
-package edu.icet.repository;
-
-public interface NativeStudentRepository {
-}
--- src/main/java/edu/icet/service/StudentServiceImpl.java
+++ src/main/java/edu/icet/service/StudentServiceImpl.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService {
